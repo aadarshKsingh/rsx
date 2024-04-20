06d1d4e4e532a9b673593e214cdd4b8e5be3ef6f
aadarshksingh <aadarshkumarsingh198@gmail.com>
2024-04-20 15:32:34 +0530

    Added pull down to update

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -2,6 +2,7 @@ import 'package:dart_rss/dart_rss.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:rsx/util.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +12,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final RefreshController _refreshContr =
+      RefreshController(initialRefresh: false);
   @override
   void initState() {
     super.initState();
@@ -20,60 +23,68 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<List<RssItem>>(
-      future: Utility().getRSS(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text(snapshot.error.toString()));
-        } else {
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              return Card(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        snapshot.data![index].title.toString(),
-                        style: const TextStyle(
-                            fontFamily: 'Gotham',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17.0),
+      body: FutureBuilder<List<RssItem>>(
+        future: Utility().getRSS(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          } else {
+            return SmartRefresher(
+              enablePullDown: true,
+              controller: _refreshContr,
+              onRefresh: () => setState(() {
+                Utility().updateRSS();
+              }),
+              child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 5.0, vertical: 5.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            snapshot.data![index].title.toString(),
+                            style: const TextStyle(
+                                fontFamily: 'Gotham',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17.0),
+                          ),
+                          Text(
+                            snapshot.data![index].pubDate
+                                .toString()
+                                .substring(0, 25),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w300, fontSize: 12.0),
+                          ),
+                          const SizedBox(
+                            height: 5.0,
+                          ),
+                          Text(
+                            Bidi.stripHtmlIfNeeded(
+                                    snapshot.data![index].content!.value)
+                                .trim(),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
                       ),
-                      Text(
-                        snapshot.data![index].pubDate
-                            .toString()
-                            .substring(0, 25),
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 12.0),
-                      ),
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                        Bidi.stripHtmlIfNeeded(
-                                snapshot.data![index].content!.value)
-                            .trim(),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        }
-      },
-    ));
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
